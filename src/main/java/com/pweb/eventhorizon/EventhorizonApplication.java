5f908e3e261aa311d5a87e372d5d5d632a9db0ad
RaresBran <rares.bran@yahoo.ro>
2024-04-06 18:31:16 +0300

    Switch to gradle and add dependencies

---
+++ src/main/java/com/pweb/eventhorizon/EventhorizonApplication.java
@@ -0,0 +1,13 @@
+package com.pweb.eventhorizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhorizonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pweb/eventhorizon/EventhorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pweb.eventhorizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
